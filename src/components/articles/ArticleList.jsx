b54d078498c75f07cb519f90718408a96a37694c
senurakoshala <koshala19299@gmail.com>
2025-02-25 09:44:30 +0530

    Dockerized the frontend

--- src/components/articles/ArticleList.jsx
+++ src/components/articles/ArticleList.jsx
@@ -1,26 +1,41 @@
-import React, { useEffect, useState } from 'react'
-import ArticleCard from './articleCard';
+import React, { useEffect, useState } from "react";
+import ArticleCard from "./ArticleCard";
 
 const ArticleList = () => {
+  const [articles, setArticle] = useState([]);
 
-    const [articles,setArticle]= useState([]);
+  const dummyArticles = [
+    {
+      id: 1,
+      title: "React Basics",
+      description: "Learn the fundamentals of React.",
+    },
+    {
+      id: 2,
+      title: "Advanced React",
+      description: "Understand hooks and performance optimizations.",
+    },
+    {
+      id: 3,
+      title: "State Management",
+      description: "Explore Redux, Context API, and more.",
+    },
+  ];
 
-    const dummyArticles = [
-        { id: 1, title: "React Basics", description: "Learn the fundamentals of React." },
-        { id: 2, title: "Advanced React", description: "Understand hooks and performance optimizations." },
-        { id: 3, title: "State Management", description: "Explore Redux, Context API, and more." },
-      ];
-
-    useEffect(()=>{
-        fetch("")
-    })
+  useEffect(() => {
+    fetch("");
+  });
   return (
     <div>
-    {dummyArticles.map((article) => (
-      <ArticleCard key={article.id} topic={article.title} description={article.description} />
-    ))}
-  </div>
-  )
-}
+      {dummyArticles.map((article) => (
+        <ArticleCard
+          key={article.id}
+          topic={article.title}
+          description={article.description}
+        />
+      ))}
+    </div>
+  );
+};
 
-export default ArticleList
+export default ArticleList;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,15 +2,14 @@ import React from "react";
 import Navbar from "../components/home/Navbar";
 import HeroSection from "../components/home/HeroSection";
 //import ArticleCard  from "../components/articles/ArticleCard.jsx";
-import ArticleList from "../components/articles/articleList.jsx";
+import ArticleList from "../components/articles/ArticleList";
 
 export default function HomePage() {
   return (
     <>
       <Navbar />
       <HeroSection />
-      <ArticleList/>
-     
+      <ArticleList />
     </>
   );
 }
