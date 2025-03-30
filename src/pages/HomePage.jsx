f929177085c4111dfc5750ba960db2076cb261f4
Parakkrama Dasanayaka <110714055+Parakkrama24@users.noreply.github.com>
2025-03-30 11:57:35 +0530

    update article list path

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "../components/home/Navbar";
 import HeroSection from "../components/home/HeroSection";
 //import ArticleCard  from "../components/articles/ArticleCard.jsx";
-import ArticleList from "../components/articles/articleList.jsx";
+import ArticleList from "../components/articles/ArticleList";
 
 export default function HomePage() {
   return (
