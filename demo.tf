7b8c8fb310be0d115985397789aab3ce380deed4
Parakkrama Dasanayaka <110714055+Parakkrama24@users.noreply.github.com>
2025-04-01 16:46:25 +0530

    tf

--- demo.tf
+++ demo.tf
@@ -1,4 +1,14 @@
 
+provider "aws" {
+  access_key = var.AWS_ACCESS_KEY
+  secret_key = var.AWS_SECRET_KEY
+  region     = "us-east-1"
+}
+
+variable "AWS_ACCESS_KEY" {}
+variable "AWS_SECRET_KEY" {}
+
+
 # -----------------------------
 # Jenkins EC2 Instance
 # -----------------------------
