42a85c4985bdf3c250ad27d511aa2e4955d82c6f
Parakkrama Dasanayaka <110714055+Parakkrama24@users.noreply.github.com>
2025-04-01 16:53:39 +0530

    tf3

--- demo.tf
+++ demo.tf
@@ -1,11 +1,4 @@
 
-provider "aws" {
-  access_key = var.AWS_ACCESS_KEY
-  secret_key = var.AWS_SECRET_KEY
-  region     = "us-east-1"
-}
-
-
 variable "AWS_ACCESS_KEY" {
   type = string
 }
@@ -13,8 +6,14 @@ variable "AWS_ACCESS_KEY" {
 variable "AWS_SECRET_KEY" {
   type = string
 }
-variable "AWS_ACCESS_KEY" {}
-variable "AWS_SECRET_KEY" {}
+provider "aws" {
+  access_key = var.AWS_ACCESS_KEY
+  secret_key = var.AWS_SECRET_KEY
+  region     = "us-east-1"
+}
+
+
+
 
 
 # -----------------------------
